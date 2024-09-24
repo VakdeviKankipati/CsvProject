34830b47d1f6d06bc7145964c0d9b18c30797eb7
Kankipati Vakdevi <vakdevikankipati@gmail.com>
2024-09-24 13:37:20 +0530

    Added Project to git

---
+++ src/test/java/com/vakya/csvfile/CsvfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vakya.csvfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
